8331c67571b29ff22e6179fb3ba259c7ca842c22
Michael O'Toole <phpbbireland@gmail.com>
2012-05-04 13:53:45 +0100

    RSS update (need version check as I can't find my last edits)

--- k2_mod_rss/root/blocks/block_rss_feeds.php
+++ k2_mod_rss/root/blocks/block_rss_feeds.php
@@ -30,8 +30,10 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-
 $queries = $cached_queries = 0;
+
+define('LEFT_SIDE', 1);
+define('RIGHT_SIDE', 2);
 
 global $k_config, $phpbb_root_path;
 
@@ -59,7 +61,7 @@ $result = $db->sql_query($sql);
 
 while ($row = $db->sql_fetchrow($result))
 {
-	if($row['feed_position'] == 1)
+	if($row['feed_position'] == LEFT_SIDE)
 	{
 		$rsleft = get_contents(1, $row['feed_url']);
 
@@ -97,9 +99,9 @@ while ($row = $db->sql_fetchrow($result))
 		));
 	}
 
-	if($row['feed_position'] == 2)
+	if($row['feed_position'] == RIGHT_SIDE)
 	{
-		$rsright = get_contents(2, $row['feed_url']);
+		$rsright = get_contents(RIGHT_SIDE, $row['feed_url']);
 
 		$msg= '<span class="gensmall"><strong>' . $rsright['description'] . "</strong></span><br />";
 
@@ -151,15 +153,12 @@ function get_contents($position, $url)
 		case 1:
 			$data = curl_get_rss($url);
 			$data = parse($data);
-
 			return($data);
-			//return(parse($data));
 		break;
 
 		case 2:
 			$data = curl_get_rss($url);
 			$data = parse($data);
-			//return(parse($data));
 			return($data);
 		break;
 
@@ -211,216 +210,208 @@ function curl_get_rss($url)
 	}
 }
 
+function parse($rss_content)
+{
+	global $rss, $user, $k_config;
+	$channeltags = array ('title', 'link', 'description', 'language', 'copyright', 'managingEditor', 'webMaster', 'lastBuildDate', 'rating', 'docs');
+	$date_format = 'U';
+	$itemtags = array('title', 'link', 'author', 'category', 'comments', 'enclosure', 'guid', 'pubDate', 'source');
+	$imagetags = array('title', 'url', 'link', 'width', 'height');
+	$textinputtags = array('title', 'description', 'name', 'link');
 
 
+	$default_cp = 'UTF-8';
+	$CDATA = 'strip';
+	$cp = 'UTF-8';
+	$rsscp = 'UTF-8';
+	$items_limit = $k_config['rss_feeds_items_limit'];
+	$stripHTML = true;
+
+	$rss_type = $k_config['rss_feeds_type'];
 
 
-
-
-
-
-	function parse($rss_content)
+	//if download was succesfull
+	if (isset($rss_content) && (sizeof($rss_content) > 0))
 	{
-		global $rss, $user, $k_config;
-		$channeltags = array ('title', 'link', 'description', 'language', 'copyright', 'managingEditor', 'webMaster', 'lastBuildDate', 'rating', 'docs');
-		$date_format = 'U';
-		$itemtags = array('title', 'link', 'author', 'category', 'comments', 'enclosure', 'guid', 'pubDate', 'source');
-		$imagetags = array('title', 'url', 'link', 'width', 'height');
-		$textinputtags = array('title', 'description', 'name', 'link');
+		// parse document encoding
+		$result['encoding'] = my_preg_match("'encoding=[\'\"](.*?)[\'\"]'si", $rss_content);
 
-
-		$default_cp = 'UTF-8';
-		$CDATA = 'strip';
-		$cp = 'UTF-8';
-		$rsscp = 'UTF-8';
-		$items_limit = $k_config['rss_feeds_items_limit'];
-		$stripHTML = true;
-
-		$rss_type = $k_config['rss_feeds_type'];
-
-
-		//if download was succesfull
-		if (isset($rss_content) && (sizeof($rss_content) > 0))
+		// if document encoding is specified, use it
+		if (isset($result['encoding']) && ($result['encoding'] != ''))
 		{
-			// parse document encoding
-			$result['encoding'] = my_preg_match("'encoding=[\'\"](.*?)[\'\"]'si", $rss_content);
+			$rsscp = $result['encoding'];
+		}
+		else
+		{
+			// otherwise use the default encoding
+			$rsscp = $default_cp;
+		}
+		// parse channel info
+		preg_match("'<channel.*?>(.*?)</channel>'si", $rss_content, $out_channel);
 
-  		  	// if document encoding is specified, use it
-			if (isset($result['encoding']) && ($result['encoding'] != ''))
+		foreach ($channeltags as $channeltag)
+		{
+			$temp = @my_preg_match("'<$channeltag.*?>(.*?)</$channeltag>'si", $out_channel[1]);
+			if (isset($temp))
 			{
-				$rsscp = $result['encoding'];
+				$result[$channeltag] = $temp;
 			}
-			else
-			{
-				// otherwise use the default encoding
-				$rsscp = $default_cp;
-			}
-			// parse channel info
-			preg_match("'<channel.*?>(.*?)</channel>'si", $rss_content, $out_channel);
+		}
 
-			foreach ($channeltags as $channeltag)
+		// If date_format is specified and lastBuildDate is valid
+		if ($date_format != '' && ($timestamp = strtotime($result['lastBuildDate'])) !== -1)
+		{
+			// convert lastBuildDate to specified date format
+			$result['lastBuildDate'] = date($date_format, $timestamp);
+		}
+
+
+		// parse textinput info
+		// This a little strange regexp means:
+		// Look for tag <textinput> with or without any attributes, but skip truncated version <textinput /> (it's not beggining tag)
+		preg_match("'<textinput(|[^>]*[^/])>(.*?)</textinput>'si", $rss_content, $out_textinfo);
+		if (isset($out_textinfo[2]))
+		{
+			foreach ($textinputtags as $textinputtag)
 			{
-				$temp = @my_preg_match("'<$channeltag.*?>(.*?)</$channeltag>'si", $out_channel[1]);
+				$temp = my_preg_match("'<$textinputtag.*?>(.*?)</$textinputtag>'si", $out_textinfo[2]);
 				if (isset($temp))
 				{
-					$result[$channeltag] = $temp;
+					$result['textinput_'.$textinputtag] = $temp;
 				}
 			}
-
-			// If date_format is specified and lastBuildDate is valid
-			if ($date_format != '' && ($timestamp = strtotime($result['lastBuildDate'])) !== -1)
+		}
+		// parse images
+		preg_match("'<image.*?>(.*?)</image>'si", $rss_content, $out_imageinfo);
+		if (isset($out_imageinfo[1]))
+		{
+			foreach ($imagetags as $imagetag)
 			{
-				// convert lastBuildDate to specified date format
-				$result['lastBuildDate'] = date($date_format, $timestamp);
-			}
-
-
-			// parse textinput info
-			// This a little strange regexp means:
-			// Look for tag <textinput> with or without any attributes, but skip truncated version <textinput /> (it's not beggining tag)
-			preg_match("'<textinput(|[^>]*[^/])>(.*?)</textinput>'si", $rss_content, $out_textinfo);
-			if (isset($out_textinfo[2]))
-			{
-				foreach ($textinputtags as $textinputtag)
+				$temp = my_preg_match("'<$imagetag.*?>(.*?)</$imagetag>'si", $out_imageinfo[1]);
+				if ($temp != '')
 				{
-					$temp = my_preg_match("'<$textinputtag.*?>(.*?)</$textinputtag>'si", $out_textinfo[2]);
+					$result['image_'.$imagetag] = $temp;
+				}
+			}
+		}
+		// parse items
+		preg_match_all("'<item(| .*?)>(.*?)</item>'si", $rss_content, $items);
+
+		//Added to read entry if format changed Ref: http://www.phpbbireland.com/phpBB3/viewtopic.php?p=16993#p16993
+		 if (empty($items[2]))
+		 {
+			preg_match_all("'<entry(| .*?)>(.*?)</entry>'si", $rss_content, $items);
+		 }
+
+
+		$rss_items = $items[2];
+		$i = 0;
+		$result['items'] = array(); // create array even if there are no items
+
+		foreach ($rss_items as $rss_item)
+		{
+			// If number of items is lower then limit: parse one item
+			if ($i < $items_limit || $items_limit == 0)
+			{
+				foreach ($itemtags as $itemtag)
+				{
+					$temp = my_preg_match("'<$itemtag.*?>(.*?)</$itemtag>'si", $rss_item);
 					if (isset($temp))
 					{
-						$result['textinput_'.$textinputtag] = $temp;
+						$result['items'][$i][$itemtag] = $temp;
 					}
 				}
-			}
-			// parse images
-			preg_match("'<image.*?>(.*?)</image>'si", $rss_content, $out_imageinfo);
-			if (isset($out_imageinfo[1]))
-			{
-				foreach ($imagetags as $imagetag)
+				// item title
+				if ($stripHTML && isset($result['items'][$i]['title']))
 				{
-					$temp = my_preg_match("'<$imagetag.*?>(.*?)</$imagetag>'si", $out_imageinfo[1]);
-					if ($temp != '')
-					{
-						$result['image_'.$imagetag] = $temp;
-					}
+					$result['items'][$i]['title'] = strip_tags(unhtmlentities(strip_tags($result['items'][$i]['title'])));
 				}
-			}
-			// parse items
-			preg_match_all("'<item(| .*?)>(.*?)</item>'si", $rss_content, $items);
 
-			//Added to read entry if format changed Ref: http://www.phpbbireland.com/phpBB3/viewtopic.php?p=16993#p16993
-	         if (empty($items[2]))
-		     {
-			    preg_match_all("'<entry(| .*?)>(.*?)</entry>'si", $rss_content, $items);
-	         }
-
-
-			$rss_items = $items[2];
-			$i = 0;
-			$result['items'] = array(); // create array even if there are no items
-
-			foreach ($rss_items as $rss_item)
-			{
-				// If number of items is lower then limit: parse one item
-				if ($i < $items_limit || $items_limit == 0)
+				// item description
+				if ($stripHTML && isset($result['items'][$i]['description']))
 				{
-					foreach ($itemtags as $itemtag)
-					{
-						$temp = my_preg_match("'<$itemtag.*?>(.*?)</$itemtag>'si", $rss_item);
-						if (isset($temp))
-						{
-							$result['items'][$i][$itemtag] = $temp;
-						}
-					}
-					// item title
-					if ($stripHTML && isset($result['items'][$i]['title']))
-					{
-						$result['items'][$i]['title'] = strip_tags(unhtmlentities(strip_tags($result['items'][$i]['title'])));
-					}
+					$result['items'][$i]['description'] = strip_tags(unhtmlentities(strip_tags($result['items'][$i]['description'])));
+				}
 
-					// item description
-					if ($stripHTML && isset($result['items'][$i]['description']))
+				// If pubDate exists
+				if ((isset($result['items'][$i]['pubDate']) && ($result['items'][$i]['pubDate'] != '') ))
+				{
+					// ... and is valid
+					if ((($timestamp = strtotime($result['items'][$i]['pubDate'])) !== -1) && (($timestamp = strtotime($result['items'][$i]['pubDate'])) === false))
 					{
-						$result['items'][$i]['description'] = strip_tags(unhtmlentities(strip_tags($result['items'][$i]['description'])));
-					}
-
-					// If pubDate exists
-					if ((isset($result['items'][$i]['pubDate']) && ($result['items'][$i]['pubDate'] != '') ))
-          			{
-						// ... and is valid
-						if ((($timestamp = strtotime($result['items'][$i]['pubDate'])) !== -1) && (($timestamp = strtotime($result['items'][$i]['pubDate'])) === false))
-						{
-							// convert pubDate to specified date format
-							$result['items'][$i]['pubDate'] = date($date_format, $timestamp);
-						}
-						else
-						{
-							$result['items'][$i]['pubDate'] = time();
-						}
+						// convert pubDate to specified date format
+						$result['items'][$i]['pubDate'] = date($date_format, $timestamp);
 					}
 					else
 					{
 						$result['items'][$i]['pubDate'] = time();
 					}
-					// item counter
-					$i++;
 				}
+				else
+				{
+					$result['items'][$i]['pubDate'] = time();
+				}
+				// item counter
+				$i++;
 			}
-			$result['items_count'] = $i;
-
-			//echo '<pre>';print_r($result);echo '</pre>';
-
-			return ($result);
 		}
-		else
-		{
-			// error in getting content return error
-			$result['items'] = array();
-			$result['description'] = $user->lang['RSS_ERROR'];
-			$result['title'] = '{L_RSS_ERROR}';
-			$result['items_count'] = '{L_RSS_ERROR}';
-			return ($result);
-		}
+		$result['items_count'] = $i;
+
+		//echo '<pre>';print_r($result);echo '</pre>';
+
+		return ($result);
 	}
-
-
-
-	function my_preg_match($pattern, $subject)
+	else
 	{
-		// start regullar expression
-		preg_match($pattern, $subject, $out);
-		// if there is some result... process it and return it
-		if (isset($out[1]))
-		{
-			// cdata
-			$out[1] = strtr($out[1], array('<![CDATA['=>'', ']]>'=>''));
-  			if ((isset($rsscp))&&($rsscp != 'UTF-8'))
-			{
-				// recode with phpBB´s functions
-				$out[1] = utf8_recode($out[1],$rsscp);
-			}
-
-			// Return result
-			return trim($out[1]);
-		}
-		else
-		{
-			// if there is NO result, return empty string
-			return('');
-		}
+		// error in getting content return error
+		$result['items'] = array();
+		$result['description'] = $user->lang['RSS_ERROR'];
+		$result['title'] = '{L_RSS_ERROR}';
+		$result['items_count'] = '{L_RSS_ERROR}';
+		return ($result);
 	}
+}
 
-    function unhtmlentities ($string)
+
+
+function my_preg_match($pattern, $subject)
+{
+	// start regullar expression
+	preg_match($pattern, $subject, $out);
+	// if there is some result... process it and return it
+	if (isset($out[1]))
 	{
-		// Get HTML entities table
-		$trans_tbl = get_html_translation_table (HTML_ENTITIES, ENT_QUOTES);
+		// cdata
+		$out[1] = strtr($out[1], array('<![CDATA['=>'', ']]>'=>''));
+		if ((isset($rsscp))&&($rsscp != 'UTF-8'))
+		{
+			// recode with phpBB´s functions
+			$out[1] = utf8_recode($out[1],$rsscp);
+		}
 
-		// Flip keys<==>values
-		$trans_tbl = array_flip ($trans_tbl);
-
-		// Add support for &apos; entity (missing in HTML_ENTITIES)
-		$trans_tbl += array('&apos;' => "'");
-
-		// Replace entities by values
-		return strtr($string, $trans_tbl);
+		// Return result
+		return trim($out[1]);
 	}
+	else
+	{
+		// if there is NO result, return empty string
+		return('');
+	}
+}
+
+function unhtmlentities ($string)
+{
+	// Get HTML entities table
+	$trans_tbl = get_html_translation_table (HTML_ENTITIES, ENT_QUOTES);
+
+	// Flip keys<==>values
+	$trans_tbl = array_flip ($trans_tbl);
+
+	// Add support for &apos; entity (missing in HTML_ENTITIES)
+	$trans_tbl += array('&apos;' => "'");
+
+	// Replace entities by values
+	return strtr($string, $trans_tbl);
+}
 
 ?>
