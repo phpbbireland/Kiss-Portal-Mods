2f48dada78f315c71c40db8eeea9483bfc6f4d69
Michael O'Toole <phpbbireland@gmail.com>
2012-05-06 22:37:29 +0200

    Update k2_mod_youtube/root/language/en/acp/k_youtube.php

--- k2_mod_youtube/root/language/en/acp/k_youtube.php
+++ k2_mod_youtube/root/language/en/acp/k_youtube.php
@@ -38,43 +38,37 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 //
 // Some characters you may want to copy&paste:
-// ’ » “ ” …
+// â€™ Â» â€œ â€ â€¦
 //
 
 $lang = array_merge($lang, array(
-	'ACP_VIDEO'				=> 'Portal videos.',
+	'ACP_K_YOUTUBE_BROWSE' 		=> 'Browse!',
+	'ACP_VIDEO'			=> 'Portal videos.',
+	'ADD_VIDEO'			=> 'Add a video',
 	'ACP_VIDEO_EXPLAIN'		=> 'Here you can add, edit and delete videos.',
-	'ACP_K_YOUTUBE_BROWSE' 	=> 'Browse!',
-
-	'VIDEO_LINK'			=> 'Video Link',
-	'VIDEO_LINK_EXPLAIN'	=> 'YouTube video link code.',
-
-	'VIDEO_RATING'				=> 'Rating',
-	'VIDEO_RATING_EXPLAIN'		=> 'Rate from 1 to 5.',
-	'VIDEO_TITLE'				=> 'Title',
-	'VIDEO_TITLE_EXPLAIN'		=> 'The title of the video.',
-	'VIDEO_WHO'					=> 'Artist',
-	'VIDEO_WHO_EXPLAIN'			=> 'Artist or details of the performers.',
-	'VIDEO_CATEGORY'			=> 'Category',
-	'VIDEO_CATEGORY_EXPLAIN'	=> 'Select from existing category or add new.',
-	'CONFIRM_OPERATION_YOUTUBE'	=> 'Delete: %s',
-
-	'VIDEO_MOVIE'			=> 'YouTube Movie',
-	'VIDEO_MOVIE_RXPLAIN'	=> 'Each YouTube can have its own movie',
-
-	'ADD_VIDEO'				=> 'Add a video',
-	'EDIT_VIDEO'			=> 'Editing video',
+	'ACP_YOUTUBE'			=> 'Youtube Video',
 	'CONFIG_VIDEO'			=> 'Config video defaults',
-
-	'ID'						=> 'ID',
 	'CONFIRM_OPERATION_VIDEO'	=> 'Delete this video?',
-	'SWITCHING'					=> 'Switching to k_config',
-
+	'CONFIRM_OPERATION_YOUTUBE'	=> 'Delete: %s',
+	'EDIT_VIDEO'			=> 'Editing video',
+	'ID'				=> 'ID',
+	'SAVED'				=> 'Saved',
+	'SWITCHING'			=> 'Switching to k_config',
+	'VIDEO_CATEGORY'		=> 'Category',
+	'VIDEO_CATEGORY_EXPLAIN'	=> 'Select from existing category or add new.',
 	'VIDEO_COMMENT'			=> 'Comments',
 	'VIDEO_CREATED'			=> 'Video created...',
+	'VIDEO_LINK'			=> 'Video Link',
+	'VIDEO_LINK_EXPLAIN'		=> 'YouTube video link code.',
+	'VIDEO_MOVIE'			=> 'YouTube Movie',
+	'VIDEO_MOVIE_RXPLAIN'		=> 'Each YouTube can have its own movie',
+	'VIDEO_RATING'			=> 'Rating',
+	'VIDEO_RATING_EXPLAIN'		=> 'Rate from 1 to 5.',
+	'VIDEO_TITLE'			=> 'Title',
+	'VIDEO_TITLE_EXPLAIN'		=> 'The title of the video.',
 	'UTUBE_SAMPLE_DATA'		=> 'I have included some sample data, just edit to suit...',
-	'SAVED'					=> 'Saved',
-
+	'VIDEO_WHO'			=> 'Artist',
+	'VIDEO_WHO_EXPLAIN'		=> 'Artist or details of the performers.',
 ));
 
 ?>
