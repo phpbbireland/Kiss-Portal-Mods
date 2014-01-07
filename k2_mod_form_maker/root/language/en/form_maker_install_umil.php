3d329960a5843c44e99c54a40c432d0fb28f2eb5
Michael O'Toole <phpbbireland@gmail.com>
2014-01-07 15:46:00 +0000

    Adding k2_mod_form_maker (requires Kiss Portal 1.0.20)

---
+++ k2_mod_form_maker/root/language/en/form_maker_install_umil.php
@@ -0,0 +1,56 @@
+<?php
+/**
+*
+* @package install
+* @version $Id: form_maker_install_umil.php 0.0.1
+* @copyright (c) 2012 phpbireland
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+
+$lang = array_merge($lang, array(
+	'ACP_FORM_MAKER'            => 'Form Maker',
+	'FORM_MAKER'                => 'Form Maker',
+	'CURRENT_VERSION'			=> 'Current version',
+	'INSTALL_INTRO'				=> 'Welcome to the Form Maker Installation <img src="./../form_marker_install.png" alt="" border="none">',
+	'INSTALL_PANEL'				=> 'Form Maker Installation Panel',
+	'LATEST_VERSION'			=> 'Latest version',
+	'NONE'						=> 'Not Installed',
+	'OVERVIEW_BODY'				=> '',
+	'REPORT_INSTALLED'			=> 'The Form Maker Mod in already installed',
+	'SELECT_LANG'				=> 'Select language',
+	'SUB_INTRO'					=> 'Introduction',
+	'SUB_SUPPORT'				=> 'Support',
+	'VERSION_CHECK'				=> 'Version check',
+	'VERSION_CHECK_EXPLAIN'		=> 'Checks to see if Form Maker version you are currently running is up to date.',
+	'VERSION_NOT_UP_TO_DATE'	=> 'Your version of the form maker is not up to date. Please continue the update process.',
+	'VERSION_NOT_UP_TO_DATEX'	=> 'Cannot retrieve version info... code not yet written.',
+));
+
+?>
---
+++ k2_mod_form_maker/root/portal_install/k2_mods/phpbb_form_mod/index.php
@@ -0,0 +1,102 @@
+<?php
+/**
+*
+* @author michaelo phpbbireland@gmail.com - http://www.phpbbireland.com
+*
+* @package form_maker
+* @version 0.0.1
+* @copyright (c) 2012 Michael O'Toole (phpbbireland.com)
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+define('UMIL_AUTO', true);
+define('IN_PHPBB', true);
+
+// correct root for poral as we install using root/portal/index.php //
+
+$phpbb_root_path = './../../../';
+
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+if (!file_exists($phpbb_root_path . 'umil/umil_auto.' . $phpEx))
+{
+	trigger_error('Please download the latest UMIL (Unified MOD Install Library) from: <a href="http://www.phpbb.com/mods/umil/">phpBB.com/mods/umil</a>', E_USER_ERROR);
+}
+
+// The name of the mod to be displayed during installation.
+$mod_name = 'FORM_MAKER';
+
+$version_config_name = 'form_maker_version';
+$language_file = 'form_maker_install_umil';
+$logo_img = 'portal_intall/k2_mods/phpbb_form_mod/form_maker_install.png';
+
+include($phpbb_root_path . 'portal_install/k2_mods/phpbb_form_mod/sql_data.' . $phpEx);
+
+$versions = array(
+
+	// Version 0.0.1
+	'0.0.1' => array(
+		'permission_add' => array(
+			array('a_form_maker', 1),
+		),
+
+		'permission_set' => array(
+			array('ROLE_ADMIN_FULL', 'a_form_maker'),
+		),
+
+		'config_add' => array(
+			array('form_maker_enabled', 1),
+		),
+
+		'table_add' => array(
+			array('phpbb_form_maker', array(
+					'COLUMNS' => array(
+						'id'				=> array('UINT', NULL, 'auto_increment'),
+						'form_id'			=> array('UINT', '0'),
+						'ndx_order'			=> array('UINT', '0'),
+						'name'				=> array('VCHAR', ''),
+						'type'				=> array('VCHAR', ''),
+						'hint'				=> array('VCHAR', ''),
+						'options'			=> array('VCHAR', ''),
+						'mandatory'			=> array('BOOL', '0'),
+					),
+					'PRIMARY_KEY'	=> 'id',
+				),
+			),
+
+		),
+
+		'module_add' => array(
+			array('acp', 'ACP_CAT_PORTAL', 'FORM_MAKER'),
+
+			array('acp', 'FORM_MAKER', array(
+					'module_basename'	=> 'form_maker',
+					'module_langname'	=> 'ACP_FORM_MAKER',
+					'module_mode'		=> 'manage',
+					'module_auth'		=> 'acl_a_form_maker',
+				),
+			),
+		),
+
+		'table_insert' => array(
+			array($form_maker_table, $form_maker_array),
+		),
+
+		// purge the cache
+		'cache_purge' => array('', 'imageset', 'template', 'theme'),
+	),
+
+);//version
+
+include($phpbb_root_path . 'umil/umil_auto.' . $phpEx);
+
+?>
